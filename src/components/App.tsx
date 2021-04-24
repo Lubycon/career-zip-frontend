c26985c0c3280490b550d9a4ac29128ddd0402f6
HyeIn Ju <hyein.ju92@gmail.com>
2021-04-24 23:56:46 +0900

    Chore: format code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,10 @@
 import { css } from '@emotion/react';
 
 const App = () => (
-  <div css={css`
-    color: blue;
-  `}
+  <div
+    css={css`
+      color: blue;
+    `}
   >
     Hello world!
   </div>
