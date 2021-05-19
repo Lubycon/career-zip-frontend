90234126a51ec286434c78f076a4ecb8fdac844a
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-19 19:36:24 +0900

    Add: Logo component

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,18 @@
+import { Box } from 'rebass';
+
+const Logo = (): JSX.Element => {
+  return (
+    <Box
+      width="300px"
+      fontWeight="bold"
+      fontSize="16px"
+      backgroundColor="blue"
+      color="#ffffff"
+      padding="10px 0"
+    >
+      Logo
+    </Box>
+  );
+};
+
+export default Logo;
