c9dbeb7113d270d458cd685f7cc4ee27b1b33363
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-19 19:36:11 +0900

    Add: loginButton component

---
+++ src/components/atoms/LoginButton.tsx
@@ -0,0 +1,25 @@
+import { css } from '@emotion/react';
+import { Link } from 'react-router-dom';
+
+interface LoginButtonProps {
+  text: string;
+}
+
+const style = css`
+  background-color: blue;
+  padding: 10px;
+  border-radius: 8px;
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const LoginButton = ({ text }: LoginButtonProps): JSX.Element => {
+  return (
+    <Link to="/login" css={style}>
+      {text}
+    </Link>
+  );
+};
+
+export default LoginButton;
