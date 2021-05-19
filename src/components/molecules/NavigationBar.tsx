55bf15ac9b215e801fef5b95c867fba54453480a
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-20 01:18:58 +0900

    Refactor: use proper variable name and add style

--- src/components/molecules/NavigationBar.tsx
+++ src/components/molecules/NavigationBar.tsx
@@ -3,12 +3,20 @@ import LoginButton from 'components/atoms/LoginButton';
 import Logo from 'components/atoms/Logo';
 import { Flex } from 'rebass';
 
-const aStyle = css`
+const navButtonStyle = css`
   display: flex;
   font-size: 15px;
   justify-content: center;
   align-items: center;
   margin-right: 40px;
+  text-decoration: none;
+  color: inherit;
+  :hover,
+  :focus,
+  :active {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
 
 const NavigationBar = () => {
@@ -16,14 +24,14 @@ const NavigationBar = () => {
     <Flex bg="lightBlue" justifyContent="space-between" padding="10px 80px">
       <Logo />
       <Flex>
-        <a href="#about" css={aStyle}>
+        <a href="#about" css={navButtonStyle}>
           About
         </a>
         <a
           href="https://www.notion.so/public-test-page-ccad85ae25ae4a0082bf3905b169c1d3"
           target="_blank"
           rel="noopener noreferrer"
-          css={aStyle}
+          css={navButtonStyle}
         >
           FAQ
         </a>
