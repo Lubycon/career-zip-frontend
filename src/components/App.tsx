5ab6b6d1bd29586ff9a41db9baf98931d839701d
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-19 19:34:43 +0900

    Add: reset css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,24 @@
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import global from 'styles/global';
+import { Global } from '@emotion/react';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-    </Switch>
-  </BrowserRouter>
+const App = (): JSX.Element => (
+  <>
+    <Global styles={global} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </>
 );
 
 export default App;
