4f797c825ac5e8e1d0419cd1259202a40f08fffd
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-19 22:52:38 +0900

    Refactor: use reset-css library

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import global from 'styles/global';
-import { Global } from '@emotion/react';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
 const App = (): JSX.Element => (
-  <>
-    <Global styles={global} />
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  </>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
