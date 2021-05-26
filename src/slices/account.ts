b29c494c22f65e60d985cd29a4cd0d861da2bca1
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-27 03:05:10 +0900

    Feat: decode jwt and store account info

--- src/slices/account.ts
+++ src/slices/account.ts
@@ -1,13 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface TAccountInfo {
+  id: number;
+  name: string;
+  job: string;
+  email: string;
+  avatarUrl: string;
+  role: string;
+}
+
+interface TInitialState extends TAccountInfo {
+  test: string;
+}
+
+const initialState: TInitialState = {
+  id: 0,
+  name: '',
+  job: '',
+  email: '',
+  avatarUrl: '',
+  role: '',
+  test: '',
+};
 
 const accountSlice = createSlice({
   name: 'account',
-  initialState: {
-    name: '',
-    email: '',
-    occupation: '',
+  initialState,
+  reducers: {
+    setAccountInfo: (state, action: PayloadAction<TAccountInfo>) => {
+      return { ...state, ...action.payload };
+    },
   },
-  reducers: {},
 });
 
+export const { setAccountInfo } = accountSlice.actions;
 export default accountSlice.reducer;
