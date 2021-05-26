a52e9cf876e505020d4e468c33a0616756e4d641
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-26 21:11:50 +0900

    Refactor: declare router in a separate file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,10 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Router from 'router';
 import store from 'slices';
-import Auth from './pages/Auth';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Main from './pages/Main';
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/auth" component={Auth} />
-        <Route path="/main" component={Main} />
-      </Switch>
-    </BrowserRouter>
+    <Router />
   </Provider>
 );
 
