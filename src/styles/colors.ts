c5b3fa7af944bfa92b062033f2206973799480ea
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-28 03:22:11 +0900

    Add: color set const

---
+++ src/styles/colors.ts
@@ -0,0 +1,24 @@
+export const BLUE = {
+  1: '#0056FF',
+  2: '#4276FF',
+  3: '#9CC0FF',
+  4: '#CFE0FF',
+};
+
+export const SKY_BLUE = {
+  1: '#1A87FF',
+  2: '#59A9FF',
+  3: '#78B8FF',
+  4: '#DEEEFF',
+};
+
+export const GRAY = {
+  1: '#191F28',
+  2: '#6B7684',
+  3: '#B0B8C1',
+  4: '#F9FAFB',
+};
+
+export const RED = '#F35750';
+
+export const GREEN = '#31CB84';
