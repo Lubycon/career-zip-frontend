29158648eeff9a2dbdc859ddef60cdfde87c3644
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-30 00:23:08 +0900

    Add: flex styles

--- src/styles/element.ts
+++ src/styles/element.ts
@@ -3,3 +3,12 @@ import { css } from '@emotion/react';
 export const logoFont = css`
   font-family: 'Open Sans', sans-serif;
 `;
+
+export const flexRow = css`
+  display: flex;
+`;
+
+export const flexColumn = css`
+  display: flex;
+  flex-direction: column;
+`;
