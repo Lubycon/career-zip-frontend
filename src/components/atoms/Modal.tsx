3a19276ebd13106a135abef538516187c5b1bb8f
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-29 18:07:16 +0900

    Fix: add padding, closable, onClose props

--- src/components/atoms/Modal.tsx
+++ src/components/atoms/Modal.tsx
@@ -1,20 +1,28 @@
 import { css } from '@emotion/react';
 
 interface ModalProps {
+  padding?: string;
+  closable: boolean;
+  onClose?: () => void;
   children: React.ReactNode;
 }
 
-const Modal = ({ children }: ModalProps) => {
+const Modal = ({ padding = '30px', closable, children, onClose }: ModalProps) => {
   return (
     <div
       css={css`
         display: flex;
-        padding: 120px 160px;
         margin: auto;
         background-color: white;
         border-radius: 20px;
+        padding: ${padding};
       `}
     >
+      {closable && (
+        <button type="button" onClick={onClose}>
+          X
+        </button>
+      )}
       {children}
     </div>
   );
