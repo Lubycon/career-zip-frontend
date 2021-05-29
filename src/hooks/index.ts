a96148baf5a5d5e134af1cc807e0d42304cc4b7d
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-30 01:40:32 +0900

    Delete: remove code and create seperate file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 import queryString from 'query-string';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useParams } from 'react-router';
 
 const useQueryStringAndParam = <T>() => {
@@ -15,22 +15,4 @@ const useQueryStringAndParam = <T>() => {
   }, [params]);
 };
 
-const useModal = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalVisible(false);
-  };
-
-  return {
-    isModalVisible,
-    handleOpenModal,
-    handleCloseModal,
-  };
-};
-
-export { useQueryStringAndParam, useModal };
+export { useQueryStringAndParam };
