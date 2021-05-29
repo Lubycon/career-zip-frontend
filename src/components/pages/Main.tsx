dd5fda9dfc754faa0f5da284d022705669d2467e
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-29 18:28:30 +0900

    Feat: show modal when user has never selected job

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,7 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import SelectJobModal from 'components/organisms/SelectJobModal';
 import MainTemplate from 'components/templates/MainTemplate';
+import { useSelector } from 'react-redux';
+import { selectAccountInfo } from 'slices/account';
 
 const Main = () => {
-  return <MainTemplate>moyaho</MainTemplate>;
+  const { job } = useSelector(selectAccountInfo);
+  const [jobModalVisible, setJobModalVisible] = useState<boolean>();
+
+  useEffect(() => {
+    const selectJobNextTime = JSON.parse(localStorage.getItem('selectJobNextTime'));
+    if (selectJobNextTime || job) {
+      setJobModalVisible(false);
+      return;
+    }
+    setJobModalVisible(true);
+  }, []);
+
+  const handleSelectNextTimeButton = () => {
+    localStorage.setItem('selectJobNextTime', 'true');
+    setJobModalVisible(false);
+  };
+
+  return (
+    <>
+      <SelectJobModal
+        visible={jobModalVisible}
+        onClickSelectNextTimeButton={handleSelectNextTimeButton}
+      />
+      <MainTemplate>moyaho</MainTemplate>
+    </>
+  );
 };
 
 export default Main;
