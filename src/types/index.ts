5a9c074f2c3b53aefb33060c96670f684a0158f1
HyeIn Ju <hyein.ju92@gmail.com>
2021-05-30 23:50:47 +0900

    Add: Archive type

--- src/types/index.ts
+++ src/types/index.ts
@@ -15,3 +15,25 @@ export interface IArchivingList {
     projects: { id: number; title: string }[];
   }[];
 }
+
+export interface IArchive {
+  id: number;
+  startDate: string;
+  endDate: string;
+  createdDateTime: string;
+  selectedProjects: { id: number; title: string }[];
+  questions: {
+    id: number;
+    priority: number;
+    inputType: string;
+    description: string;
+    example: string | null;
+    selectOptions: string[];
+    anwsers: {
+      id: number;
+      comment: string;
+      important: number;
+      project: { id: number; title: string };
+    }[];
+  }[];
+}
