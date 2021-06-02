0a94a7fe0cb4fb328602cff8f154fc3e890ae73b
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-03 04:44:45 +0900

    Refactor: use HomeButton component

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,36 +1,15 @@
-import { Link } from 'react-router-dom';
 import { css } from '@emotion/react';
-import linkStyle from 'styles/link';
 import LoginPageBackground from 'components/atoms/LoginPageBackground';
 import LoginModalContent from 'components/organisms/LoginModalContent';
 import { Flex } from 'rebass';
 import { modal } from 'styles/element';
-
-const homeButton = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100px;
-  height: 32px;
-  font-size: 18px;
-  font-weight: bold;
-  border-radius: 5px;
-  background: rgba(122, 160, 255, 0.5);
-  color: #fff;
-  ${linkStyle}
-
-  >img {
-    margin-right: 10px;
-  }
-`;
+import HomeButton from 'components/molecules/HomeButton';
 
 const Login = () => {
   return (
     <LoginPageBackground>
       <Flex width="1440px" padding="30px 0" height="100%" margin="auto" flexDirection="column">
-        <Link to="/" css={homeButton}>
-          <img src="/public/icons/left_arrow.svg" alt="left arrow" /> 홈으로
-        </Link>
+        <HomeButton />
         <div
           css={css`
             ${modal};
