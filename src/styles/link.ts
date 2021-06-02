09973ba02b2c5e062999ebbd2d5f84316b452b0a
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-03 03:21:21 +0900

    Add: add main login button style

--- src/styles/link.ts
+++ src/styles/link.ts
@@ -1,14 +1,28 @@
 import { css } from '@emotion/react';
+import { BLUE } from './colors';
 
 const linkStyle = css`
   text-decoration: none;
-  color: inherit;
   :hover,
   :focus,
   :active {
     text-decoration: none;
-    color: inherit;
   }
+`;
+
+export const mainLoginButton = css`
+  display: flex;
+  width: 500px;
+  padding: 16px 0;
+  background-color: ${BLUE[1]};
+  border-radius: 10px;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+  margin-top: 24px;
+  font-weight: bold;
+  box-shadow: 0px 0px 15px 5px #104ef24d;
+  ${linkStyle}
 `;
 
 export default linkStyle;
