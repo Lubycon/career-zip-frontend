4f6a5f23513fbc286e4d38da32b050d102b408f7
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-03 04:42:58 +0900

    Add: feedback form url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,4 +3,5 @@ export const externaURL = {
   instagram: 'https://www.notion.so/public-test-page-ccad85ae25ae4a0082bf3905b169c1d3',
   FAQ: 'https://www.notion.so/Career-zip-FAQ-b00c33acbbb14e9e8efed93c304bc5b1 ',
   updateNote: 'https://www.notion.so/Career-zip-Update-Notes-83f80021918d4c17b2c758ed99d0c1d8',
+  feedbackForm: 'https://forms.gle/WDN8VFbW4spv4RNe8',
 };
