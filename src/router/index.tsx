13bfab30e82c40aaee09ac53b62224fb31bc89bb
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-04 00:53:41 +0900

    Feat: add not found page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import Auth from 'components/pages/Auth';
 import Home from 'components/pages/Home';
 import Login from 'components/pages/Login';
 import TempMain from 'components/pages/TempMain';
+import NotFoundPage from 'components/pages/NotFoundPage';
 // import ArchivingList from 'components/pages/ArchivingList';
 // import CareerzipReport from 'components/pages/CareerzipReport';
 // import ProjectManagement from 'components/pages/ProjectManagement';
@@ -16,6 +17,7 @@ const Router = () => {
         <Route path="/login" component={Login} />
         <Route path="/auth" component={Auth} />
         <Route path="/main" component={TempMain} />
+        <Route component={NotFoundPage} />
         {/* <Route path="/archiving-list" component={ArchivingList} />
         <Route path="/archive/:id" component={Archive} />
         <Route path="/careerzip-report" component={CareerzipReport} />
