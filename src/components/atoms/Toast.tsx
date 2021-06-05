eb43d6b3b79db484b63cd17de9a025fb2c07ab40
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-05 13:05:13 +0900

    Add: toast component

---
+++ src/components/atoms/Toast.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import { ReactNode } from 'react';
+
+const Wrapper = styled.div`
+  position: fixed;
+  width: 100vw;
+  display: flex;
+  top: -70px;
+  transition: top 350ms;
+
+  &.show {
+    top: 5%;
+  }
+`;
+
+const ToastContent = styled.div`
+  display: inline-block;
+  margin: auto;
+  text-align: center;
+  padding: 22px;
+  background-color: #ffffff;
+  border-radius: 20px;
+  box-shadow: 0px 0px 30px 0px rgba(196, 196, 196, 0.4);
+`;
+
+const Toast = ({ isVisible, children }: { isVisible: boolean; children: ReactNode }) => (
+  <Wrapper className={isVisible ? 'show' : undefined}>
+    <ToastContent>{children}</ToastContent>
+  </Wrapper>
+);
+
+export default Toast;
