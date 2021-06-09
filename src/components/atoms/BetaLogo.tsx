f441ded71750a84a8374428c3e0e5641b4db1a7d
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-10 02:05:53 +0900

    Add: beta version logo component

--- src/components/atoms/BetaLogo.tsx
+++ src/components/atoms/BetaLogo.tsx
@@ -1,3 +1,10 @@
-const BetaLogo = () => <img alt="beta logo" src="/public/images/beta-logo.svg" />;
+interface BetoLogoProps {
+  width?: string;
+  height?: string;
+}
+
+const BetaLogo = ({ width = 'auto', height = 'auto' }: BetoLogoProps) => (
+  <img alt="beta logo" width={width} height={height} src="/public/images/beta-logo.svg" />
+);
 
 export default BetaLogo;
