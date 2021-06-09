ea55bbfbbbc2530b6eb5951316bdd5a4fbb00c8a
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-10 02:14:42 +0900

    Fix: render Main component

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Auth from 'components/pages/Auth';
 import Home from 'components/pages/Home';
 import Login from 'components/pages/Login';
-import TempMain from 'components/pages/TempMain';
+import Main from 'components/pages/Main';
 import NotFoundPage from 'components/pages/NotFoundPage';
 // import ArchivingList from 'components/pages/ArchivingList';
 // import CareerzipReport from 'components/pages/CareerzipReport';
@@ -16,7 +16,7 @@ const Router = () => {
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/auth" component={Auth} />
-        <Route path="/main" component={TempMain} />
+        <Route path="/main" component={Main} />
         <Route component={NotFoundPage} />
         {/* <Route path="/archiving-list" component={ArchivingList} />
         <Route path="/archive/:id" component={Archive} />
