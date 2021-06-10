805e55b0fca145567b87a2f2742df8291108859e
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-11 02:16:07 +0900

    Delete: delete code no longer used

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,34 +1,8 @@
-import React, { useEffect } from 'react';
-import useModal from 'hooks/useModal';
-import SelectJobModalContent from 'components/organisms/SelectJobModalContent';
 import MainTemplate from 'components/templates/MainTemplate';
-import { useSelector } from 'react-redux';
-import { RootState } from 'slices';
-import { getLocalStorageItem, setLocalStorageItem } from 'utils/localstorage';
 
 const Main = () => {
-  const job = useSelector((state: RootState) => state.account.job);
-  const { handleOpenModal, handleCloseModal, renderModal } = useModal({});
-
-  useEffect(() => {
-    const selectJobNextTime = getLocalStorageItem<boolean>('selectJobNextTime');
-    if (selectJobNextTime || job) {
-      handleCloseModal();
-      return;
-    }
-    handleOpenModal();
-  }, []);
-
-  const handleClickSelectNextTimeButton = () => {
-    setLocalStorageItem<boolean>('selectJobNextTime', true);
-    handleCloseModal();
-  };
-
   return (
     <>
-      {renderModal(
-        <SelectJobModalContent onClickSelectNextTimeButton={handleClickSelectNextTimeButton} />
-      )}
       <MainTemplate>moyaho</MainTemplate>
     </>
   );
