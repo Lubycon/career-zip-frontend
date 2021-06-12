11cff4b19c835bfa5bfcaad7f9e8bd9ece6bfd56
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-13 04:22:14 +0900

    Add: archive period component

---
+++ src/components/atoms/ArchivePeriod.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import { GRAY } from 'styles/colors';
+
+const ArchivePeriod = styled.span`
+  font-size: 32px;
+  font-weight: bold;
+  color: ${GRAY[1]};
+  line-height: 44.8px;
+`;
+
+export default ArchivePeriod;
