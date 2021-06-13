aa53c990c3e9c635e251976a3a2765cf7b58911d
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-14 01:30:18 +0900

    Refactor: use imported component

--- src/components/molecules/QuestionBlock.tsx
+++ src/components/molecules/QuestionBlock.tsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { Flex, Text } from 'rebass';
 import { GRAY } from 'styles/colors';
 import { IProject, IQuestion } from 'types';
 import { debounce } from 'lodash';
-import Badge from 'components/atoms/Badge';
-
-const AnswerBlockWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 15px 12px;
-  border: 1px solid ${GRAY[3]};
-  border-radius: 10px;
-
-  :not(:first-of-type) {
-    margin-top: 40px;
-  }
-`;
-
-const StyledTextArea = styled.textarea`
-  margin-top: 5px;
-  border: none;
-  font-size: 14px;
-  color: ${GRAY[1]};
-  resize: none;
-  outline: none;
-
-  :placeholder {
-    color: ${GRAY[3]};
-  }
-`;
+import AnswerBlock from 'components/molecules/AnswerBlock';
+import TextArea from 'components/atoms/TextArea';
 
 interface ArchiveAnswerBlockProps {
   question: IQuestion;
@@ -37,13 +12,13 @@ interface ArchiveAnswerBlockProps {
   onChangeTextArea: (questionId: number, projectId: number, value: string) => void;
 }
 
-interface AnswerBlockProps {
+interface AnswerBlockWrapperProps {
   questionId: number;
   project: IProject;
   onChange: (questionId: number, projectId: number, value: string) => void;
 }
 
-const AnswerBlock = ({ questionId, project, onChange }: AnswerBlockProps) => {
+const AnswerBlockWrapper = ({ questionId, project, onChange }: AnswerBlockWrapperProps) => {
   const debounced = debounce(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       onChange(questionId, project.id, e.target.value);
@@ -59,19 +34,20 @@ const AnswerBlock = ({ questionId, project, onChange }: AnswerBlockProps) => {
   };
 
   return (
-    <AnswerBlockWrapper>
-      <Badge>{`# ${project.title}`}</Badge>
-      <Flex flexDirection="column" margin="16px 28px">
-        <Text fontSize="16px" fontWeight="bold" color={GRAY[1]}>
-          이번주의 커리어 아카이빙
-        </Text>
-        <StyledTextArea
-          placeholder="이곳에 프로젝트를 기록해보세요."
-          name="text"
-          onChange={(e) => handleChange(e)}
-        />
-      </Flex>
-    </AnswerBlockWrapper>
+    <AnswerBlock projectTitle={project.title}>
+      <>
+        <Flex flexDirection="column" margin="16px 28px">
+          <Text fontSize="16px" fontWeight="bold" color={GRAY[1]}>
+            이번주의 커리어 아카이빙
+          </Text>
+          <TextArea
+            placeholder="이곳에 프로젝트를 기록해보세요."
+            name="text"
+            onChange={(e) => handleChange(e)}
+          />
+        </Flex>
+      </>
+    </AnswerBlock>
   );
 };
 
@@ -92,7 +68,7 @@ const QuestionBlock = ({
       >{`${priority}. ${description}`}</Text>
       <Flex flexDirection="column" margin="20px 0 0 0">
         {selectedProjects.map((project) => (
-          <AnswerBlock
+          <AnswerBlockWrapper
             key={project.id}
             questionId={question.id}
             project={project}
