4f305b4e34eb127a938f8e4da59a9412644595f2
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-14 01:48:06 +0900

    Style: make sidebar always stick to the top

--- src/components/templates/MainTemplate.tsx
+++ src/components/templates/MainTemplate.tsx
@@ -6,7 +6,7 @@ const MainTemplate = ({ children }: { children: React.ReactNode }) => {
   return (
     <Flex minHeight="100vh" maxHeight="100vh">
       <SideMenu />
-      <Flex flexDirection="column" flex="1">
+      <Flex flexDirection="column" flex="1" maxHeight="100vh" overflowY="auto">
         <GNB />
         <Box padding="10px 240px 147px 160px">{children}</Box>
       </Flex>
