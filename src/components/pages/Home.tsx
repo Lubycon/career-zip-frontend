b550652e0aabe0ea35da66826cfef2465abaafed
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-16 02:29:23 +0900

    Feat: show modal when mobile device is detected

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,19 +1,43 @@
+import React, { useEffect } from 'react';
 import { Flex } from 'rebass';
+import useModal from 'hooks/useModal';
 import HomeMainSection from 'components/molecules/HomeMainSection';
 import HomeShareSection from 'components/molecules/HomeShareSection';
 import HomeSubSection from 'components/molecules/HomeSubSection';
 import NavigationBar from 'components/molecules/NavigationBar';
 import LandingPageFooter from 'components/organisms/LandingPageFooter';
+import CloseButton from 'components/atoms/CloseButton';
+import MobileDeviceModalContent from 'components/organisms/MobileDeviceModalContent';
 
 const Home = () => {
+  const { userAgent } = navigator;
+  const isMobile = /Android|webOS|iPhone|iPad|iPod/i.test(userAgent);
+  const { handleOpenModal, handleCloseModal, renderModal } = useModal({
+    width: '280px',
+    padding: '14px 16px',
+    borderRadius: '10px',
+  });
+
+  useEffect(() => {
+    if (isMobile) {
+      handleOpenModal();
+    }
+  }, [isMobile]);
+
   return (
-    <Flex flexDirection="column">
-      <NavigationBar />
-      <HomeMainSection />
-      <HomeSubSection />
-      <HomeShareSection />
-      <LandingPageFooter />
-    </Flex>
+    <>
+      {renderModal(
+        <MobileDeviceModalContent />,
+        <CloseButton width="15px" height="15px" onClick={handleCloseModal} />
+      )}
+      <Flex flexDirection="column" minWidth="1440px">
+        <NavigationBar />
+        <HomeMainSection />
+        <HomeSubSection />
+        <HomeShareSection />
+        <LandingPageFooter />
+      </Flex>
+    </>
   );
 };
 
