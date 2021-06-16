3b2c1d6f1e4e683e1a1bbcb2dd9ab704988571ff
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-16 20:56:21 +0900

    Fix: add 419 status code and remove localstorage

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: `${process.env.ENV_DEV_BASE_URL}/${process.env.ENV_DEV_VERSION}`,
+  // baseURL: `${process.env.ENV_DEV_BASE_URL}/${process.env.ENV_DEV_VERSION}`,
+  baseURL: `https://local.career-zip.com/v1`,
 });
 
 client.interceptors.response.use(
@@ -10,9 +11,13 @@ client.interceptors.response.use(
   },
   (error) => {
     if (error.response.status === 401) {
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('user');
       window.location.href = '/login?error=unauthorized';
     }
-    if (error.response.status === 403) {
+    if (error.response.status === 403 || error.response.status === 419) {
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('user');
       window.location.href = '/login?error=expired-auth';
     }
     return Promise.reject(error);
