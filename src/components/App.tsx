5daae37b38b130b957257891728c6e66481ab819
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-17 02:11:06 +0900

    Add: initiate amplitude

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Global } from '@emotion/react';
 import { Provider } from 'react-redux';
 import Router from 'router';
 import store from 'slices';
 import global from 'styles/global';
 import { ToastProvider } from 'context/Toast';
+import { logger } from '@lubycon/utils';
+import { isProduction } from 'utils/misc';
 
-const App = () => (
-  <>
-    <Global styles={global} />
-    <Provider store={store}>
-      <ToastProvider>
-        <Router />
-      </ToastProvider>
-    </Provider>
-  </>
-);
+const App = () => {
+  useEffect(() => {
+    logger.init({
+      services: {
+        amplitude: process.env.AMPLITUDE_APP_KEY,
+      },
+      mode: isProduction ? 'production' : 'development',
+    });
+  }, []);
+
+  return (
+    <>
+      <Global styles={global} />
+      <Provider store={store}>
+        <ToastProvider>
+          <Router />
+        </ToastProvider>
+      </Provider>
+    </>
+  );
+};
 
 export default App;
