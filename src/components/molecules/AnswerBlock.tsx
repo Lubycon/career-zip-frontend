f0db0b2d68a822586b9d799a5f118040c4a8671a
HyeIn Ju <hyein.ju92@gmail.com>
2021-06-17 23:46:24 +0900

    Style: change border color

--- src/components/molecules/AnswerBlock.tsx
+++ src/components/molecules/AnswerBlock.tsx
@@ -1,13 +1,13 @@
 import styled from '@emotion/styled';
 import Badge from 'components/atoms/Badge';
 import React from 'react';
-import { GRAY } from 'styles/colors';
+import { LIGHT_GRAY } from 'styles/colors';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 15px 12px;
-  border: 1px solid ${GRAY[3]};
+  border: 1px solid ${LIGHT_GRAY[2]};
   border-radius: 10px;
 
   :not(:first-of-type) {
